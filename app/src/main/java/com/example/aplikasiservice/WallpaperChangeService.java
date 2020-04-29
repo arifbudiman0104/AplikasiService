f977c9a766b41332787603d4763d205723ea539d
Arif Budiman Arrosyid <arif.budiman.ft18@mail.umy.ac.id>
2020-04-29 11:25:16 +0700

    FIX EROR

--- app/src/main/java/com/example/aplikasiservice/WallpaperChangeService.java
+++ app/src/main/java/com/example/aplikasiservice/WallpaperChangeService.java
@@ -2,14 +2,13 @@ package com.example.aplikasiservice;
 
 import android.app.Service;
 import android.app.WallpaperManager;
-import android.os.Bundle;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.content.Intent;
+import android.os.Bundle;
 import android.os.IBinder;
-import java.io.IOException;
 
-public class WallpaperChangeService extends Service implements Runnable{
+public class WallpaperChangeService  extends Service implements Runnable{
     private int wallpaperId[] = {R.drawable.wallpaper1, R.drawable.wallpaper2};
     private int waktu;
     private int FLAG=0;
@@ -17,20 +16,22 @@ public class WallpaperChangeService extends Service implements Runnable{
     private Bitmap gambar;
     private Bitmap gambar1;
 
-
     @Override
-    public IBinder onBind (Intent arg0){
+    public IBinder onBind(Intent arg0){
         return null;
     }
-
     @Override
-    public int onStartCOmmand(Intent intent, int flag, int startId)
+    public int onStartCommand(Intent intent, int flag, int startId)
     {
         super.onStartCommand(intent, flag, startId);
+
         Bundle bundle=intent.getExtras();
-        waktu=bundle.getInt("durasi");
-        gambar=BitmapFactory.decodeResource(getResources(),wallpaperId[0]);
-        gambar1=BitmapFactory.decodeResource(getResources(),wallpaperId[1]);
+
+        waktu = bundle.getInt("durasi");
+
+        gambar= BitmapFactory.decodeResource(getResources(),wallpaperId[0]);
+        gambar1=BitmapFactory.decodeResource(getResources(), wallpaperId[1]);
+
         t = new Thread(WallpaperChangeService.this);
         t.start();
         return START_STICKY_COMPATIBILITY;
@@ -40,23 +41,25 @@ public class WallpaperChangeService extends Service implements Runnable{
         super.onDestroy();
         System.exit(0);
     }
+
     @Override
-    public void run(){
-        WallpaperManager mywallpaper;
+    public void run() {
+        WallpaperManager myWallpaper;
         myWallpaper = WallpaperManager.getInstance(this);
-        try{
-            while(true){
-                if(FLAG==0){
+        try {
+            while (true) {
+                if (FLAG==0) {
                     myWallpaper.setBitmap(gambar);
                     FLAG++;
-                }else{
+                }
+                else {
                     myWallpaper.setBitmap(gambar1);
                     FLAG--;
                 }
                 Thread.sleep(100*waktu);
-        }
-    } catch (Exception e){
+            }
+        } catch (Exception e) {
             e.printStackTrace();
+        }
     }
-
 }
